4077964da564d107eb24ed0b50fb9eedc4e03a86
3ryooo <183440614+3ryooo@users.noreply.github.com>
2025-05-29 17:11:16 +0900

    finish: https://www.hackingwithswift.com/books/ios-swiftui/creating-a-detail-view-for-resorts

--- SnowSeeker/Views/ContentView.swift
+++ SnowSeeker/Views/ContentView.swift
@@ -34,6 +34,10 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Resorts")
+            .navigationDestination(for: Resort.self) { resort in
+                ResortView(resort: resort)
+            }
+
         } detail: {
             WelcomeView()
         }
