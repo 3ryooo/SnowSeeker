6b16590ab8b1f8b2c836f29ba3ab0b6fbbbb535c
3ryooo <183440614+3ryooo@users.noreply.github.com>
2025-05-29 13:51:11 +0900

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,15 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
